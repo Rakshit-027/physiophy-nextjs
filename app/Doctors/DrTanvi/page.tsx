7cce3bb8f9263e9c0cb42d5b5bb356e01d88b3bf
Rakshit waghmare <rakshitwaghmare@gmail.com>
2025-06-05 17:54:35 +0530

    Update page.tsx

--- app/Doctors/DrTanvi/page.tsx
+++ app/Doctors/DrTanvi/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import DrTanviViewMore from './DrTanviViewMore';
 
 export const metadata = {
-  title: "Dr. Tanvi Geet Kataria",
+  title: "Dr. Tanvi Geet Katariya",
   description: "MPTH Neuro | Founder, PhysioPhy",
   openGraph: {
     images: ["https://zlmsmdibvnnhxthvdhhf.supabase.co/storage/v1/object/public/ScrollSlider/docs/cropped_image%20(1).png"],
